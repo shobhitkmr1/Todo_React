03680b5df854029009d348e3871153b538ca2f65
Shobhitkmr <43270097+shobhitkmr1@users.noreply.github.com>
2022-10-04 11:11:34 +0530

    form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './components/Forms';
+import Form from './components/Forms';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Ed's Todo List</h1>
       </header>
+      <Form/>
     </div>
   );
 }
